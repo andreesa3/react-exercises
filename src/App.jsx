4ab2df3a9d7cfd9cedf83629b18499a0e2ce041b
Andrea <andrea.saitta.as@gmail.com>
2023-10-04 21:45:56 +0200

    ex11

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Hello from "./Hello"
+
 function App() {
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Hello />
     </>
   )
 }
---
+++ src/Hello.jsx
@@ -0,0 +1,10 @@
+const Hello = () => {
+  return (
+    <>
+      <h2>Hello, World!</h2>
+      <p>What a beautiful day!</p>
+    </>
+  )
+}
+
+export default Hello;
