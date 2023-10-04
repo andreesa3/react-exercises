731cc875e0891bb4976e7ada08bd12bbfa282122
Andrea <andrea.saitta.as@gmail.com>
2023-10-04 14:56:34 +0200

    ex3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
 
+  // The sayHello function expects a 'name' parameter, but if it's not provided,
+  // 'name' will be undefined.
   const sayHello = (name) => {
-    return <h1>Hello {name}</h1>
+    return <h1>Hello {name}</h1>;
   }
 
   return (
     <>
-    {/* La variabile name non appare a schermo ma abbiamo solo "Hello " con spazio vuoto*/}
-    {sayHello()}
+    {/* The 'name' parameter is not provided here, so it will be undefined. */}
+    {sayHello()} {/* This will render "Hello undefined" */}
     </>
   )
 }
