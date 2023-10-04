c8dd585e0ac0d7c0bf4f0825b95c47312bfb6907
Andrea <andrea.saitta.as@gmail.com>
2023-10-04 14:53:07 +0200

    ex3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 function App() {
+
   const sayHello = (name) => {
     return <h1>Hello {name}</h1>
   }
 
   return (
     <>
-    {sayHello('Andrea')}
+    {/* La variabile name non appare a schermo ma abbiamo solo "Hello " con spazio vuoto*/}
+    {sayHello()}
     </>
   )
 }
