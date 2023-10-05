4805ee3a35f055a863a60d3fcd51f3c1d5bd67c8
Andrea <andrea.saitta.as@gmail.com>
2023-10-05 10:44:19 +0200

    ex17

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,4 +1,5 @@
 const Welcome = ({name = "MARIO"}) => {
+  // Se non impostiamo nulla come valore predefinito non verrà mandato nulla a schermo poiché undefined
   return (
     <>
       <h1>Welcome {name}</h1>
