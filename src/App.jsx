ea1618574d4ebf5c377e424ed4a80e48ce981280
Andrea <andrea.saitta.as@gmail.com>
2023-10-05 11:02:48 +0200

    ex4

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import { useState } from 'react'
 
 function App() {
 
-  const sayHello = (name) => {
-    return <h1>Hello {name}</h1>
-  }
+  const name = <h1>Andrea</h1>
 
   return (
     <>
-    {/* L'espressione JSX verrà interpretata da React e mandata a schermo
-     come figlio diretto di h1 */}
-    {sayHello(<span>Andrea</span>)}
+      {name}
     </>
   )
 }
