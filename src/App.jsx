6523b13e7b9f7a968c99a15ee2fd639d08b4df1c
Andrea <andrea.saitta.as@gmail.com>
2023-10-08 17:07:30 +0200

    ex40

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import MultiButton from "./MultiButton";
 
 export default function App () {
-  const printName = (e) => {
-    console.log(e.target.name);
-  }
   return (
     <>
       <MultiButton name1="one" name2="two" name3="three"/>
