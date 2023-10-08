0ab0dd836200eb07b7c4c6668670d9baa3d2980c
Andrea <andrea.saitta.as@gmail.com>
2023-10-08 16:04:23 +0200

    ex38

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -4,11 +4,17 @@ export default function Counter() {
   const printName = (e) => {
     console.log(e.currentTarget.name);
   }
+  const printSrc = (e) => {
+    console.log(e.currentTarget.src);
+  }
 
   return (
     <>
+      {/* Abbiamo sia il name che il src perché avendo utilizzato il currentTarget 
+      mandiamo in console l'elemento a cui è stato affidato il gestore dell'evento, 
+      in questo caso entrambi ne hanno uno, quindi vengono mostrati tutti e due */}
       <button name="one" onClick={printName}>
-        <img src={btnImage} width={50} height={30}/>
+        <img onClick={printSrc} src={btnImage} width={50} height={30} />
       </button>
     </>
   )
