56ae9afd42852258b67fe3997507e4011eda66ab
Andrea <andrea.saitta.as@gmail.com>
2023-10-08 17:06:09 +0200

    ex40

--- src/MultiButton.jsx
+++ src/MultiButton.jsx
@@ -1,14 +1,12 @@
-import btnImage from './icons/black-next.svg'
-
-export default function Counter() {
+export default function MultiButton({ name1, name2, name3 }) {
   const printName = (e) => {
     console.log(e.target.name);
   }
   return (
     <>
-      <button name="one" onClick={printName}>Cliccami</button>
-      <button name="two" onClick={printName}>Cliccami</button>
-      <button name="three" onClick={printName}>Cliccami</button>
+      <button name={name1} onClick={printName}>One</button>
+      <button name={name2} onClick={printName}>Two</button>
+      <button name={name3} onClick={printName}>Three</button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MultiButton from './MultiButton'; 
+import App from './App'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultiButton />
+    <App />
   </React.StrictMode>
 )
