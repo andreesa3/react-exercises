fb41b681f2c52a622f4f0caa12ebefa4b89e55e7
Andrea <andrea.saitta.as@gmail.com>
2023-10-12 11:33:46 +0200

    ex43

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Login from "./Login"
 
 const App = () => {
-  const onLogin = () => {
+
+  const onLogin = (e, data) => {
+    e.preventDefault();
     console.log('data', data)
   }
 
