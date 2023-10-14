4f149b8cdc29fcc55df5dd9cad19868eac48aa8f
Andrea <andrea.saitta.as@gmail.com>
2023-10-14 09:59:57 +0200

    ex48

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -1,10 +1,16 @@
 const UncontrolledLogin = () => {
   const handleFormData = (e) => {
     e.preventDefault();
-    
+
+    // Standard
     // const username = e.target.elements.namedItem('username').value;
     // const password = e.target.elements.namedItem('password').value;
     // const remember = e.target.elements.namedItem('remember').checked;
+    
+    // Non standard
+    // const username = e.target.username.value;
+    // const password = e.target.password.value;
+    // const remember = e.target.remember.checked;
 
     // const data = {
     //   username,
@@ -12,13 +18,12 @@ const UncontrolledLogin = () => {
     //   remember
     // }
 
+    const formData = new FormData(e.target);
 
-    const formData = new FormData(e.target)
-    
     const data = {
       username: formData.get('username'),
       password: formData.get('password'),
-      remember: formData.get('remember') === "on" ? true : false
+      remember: formData.get('remember') === 'on' ? true : false
     }
 
     console.log(data);
