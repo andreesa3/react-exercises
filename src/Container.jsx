3642436672e7afda1bc904113866704e91a4439e
andreesa3 <90782556+andreesa3@users.noreply.github.com>
2023-10-18 11:00:23 +0200

    Container.jsx

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,7 +1,7 @@
 const Container = ({children}) => {
   return (
     <>
-      <div className="app bg-white-400 border border-red-700 p-10 m-5">
+      <div className="app bg-white border border-red-700 p-10 m-5">
         {children}
       </div>
     </>
