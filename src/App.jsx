534f48b1bafd1b8f48c5b6f07104e2780e59f88f
Andrea <andrea.saitta.as@gmail.com>
2023-10-18 11:03:56 +0200

    ex68

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Container from "./Container";
 const App = () => {
   return (
     <>
-      <Container>
-        <h1 className="text-100 text-3xl font-extrabold border-b-2 mb-4 py-2">My Bio</h1>
+      <Container title={<h1 className="text-100 text-3xl font-extrabold border-b-2 mb-4 py-2">My Blog</h1>}>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis necessitatibus iusto ea pariatur ab assumenda hic, fuga delectus labore id possimus voluptatem facilis architecto eum sunt mollitia magnam amet? Exercitationem?</p>
         <br />
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis necessitatibus iusto ea pariatur ab assumenda hic, fuga delectus labore id possimus voluptatem facilis architecto eum sunt mollitia magnam amet? Exercitationem?</p>
--- src/Container.jsx
+++ src/Container.jsx
@@ -1,8 +1,9 @@
-const Container = ({children}) => {
+const Container = ({ title, children }) => {
   return (
     <>
-      <div className="app bg-white-400 border border-red-700 p-10 m-5">
-        {children}
+      <div className="app bg-white border border-red-700 p-10 m-5">
+        <div className="container-title">{title}</div>
+        <div className="container-content">{children}</div>
       </div>
     </>
   )
