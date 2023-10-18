03146251979b825ed693fe1e453a6cf16c3acf20
Andrea <andrea.saitta.as@gmail.com>
2023-10-18 11:09:08 +0200

    ex69

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const Container = ({ title, children }) => {
+  const [collapse, setCollapse] = useState(false);
+
+  const handleCollapse = () => {
+    setCollapse(c => !c);
+  }
+
   return (
     <>
       <div className="app bg-white border border-red-700 p-10 m-5">
-        <div className="container-title">{title}</div>
-        <div className="container-content">{children}</div>
+        <div className="container-title cursor-pointer" onClick={handleCollapse}>{title}</div>
+        {
+          collapse && <div className="container-content">{children}</div>
+        }
       </div>
     </>
   )
