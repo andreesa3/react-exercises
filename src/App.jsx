1468e4f416f75d6b859b17085fffe0bf2922f2bd
Andrea <andrea.saitta.as@gmail.com>
2023-10-18 23:35:10 +0200

    ex70

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import LanguageContext from "./LanguageContext";
 import Container from "./Container";
+import Clock from './Clock';
+import { useState } from "react";
 
 const App = () => {
+  const [language, setLanguage] = useState('en');
+
+  const handleLanguageSelector = (e) => {
+    setLanguage(e.target.value)
+  }
+
+
   return (
     <>
-      <Container title={<h1 className="text-100 text-3xl font-extrabold border-b-2 mb-4 py-2">My Blog</h1>}>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis necessitatibus iusto ea pariatur ab assumenda hic, fuga delectus labore id possimus voluptatem facilis architecto eum sunt mollitia magnam amet? Exercitationem?</p>
-        <br />
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis necessitatibus iusto ea pariatur ab assumenda hic, fuga delectus labore id possimus voluptatem facilis architecto eum sunt mollitia magnam amet? Exercitationem?</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti distinctio dolore sapiente porro expedita cupiditate saepe esse harum recusandae accusamus. Nulla tempore doloribus suscipit dignissimos. Eos culpa in voluptatibus cum impedit, corrupti illo quaerat labore accusantium molestias ad laudantium sequi esse, illum perspiciatis fuga itaque numquam, hic porro aut voluptatum. Recusandae enim fugiat asperiores impedit numquam eveniet nam? Quibusdam, tenetur. Veniam omnis laborum inventore eaque ullam odit fugiat perferendis sit neque id quos temporibus facere quibusdam doloremque sint dolores, tenetur cumque illum animi harum? Deleniti nemo quaerat aliquam architecto reprehenderit sed! Quibusdam eveniet ut necessitatibus obcaecati corrupti temporibus, veritatis ducimus.</p>
-      </Container>
+      <div>
+        <select name="languageSelector" id="" onChange={handleLanguageSelector}>
+          <option value="it">IT</option>
+          <option value="en">EN</option>
+          <option value="de">DE</option>
+        </select>
+        <Container title={<h1 className="text-3xl font-extrabold border-b-2 mb-4 py-2">My Blog</h1>}>
+          <LanguageContext.Provider value={language}>
+            <Clock />
+          </LanguageContext.Provider>
+        </Container>
+      </div>
     </>
   )
 }
---
+++ src/Clock.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { useState, useEffect } from "react"
+import LanguageContext from "./LanguageContext";
+
+const Clock = ({}) => {
+  const [time, setTime] = useState('');
+
+  const language = useContext(LanguageContext)
+
+  const currentTime = () => {
+    return setTime(new Date().toLocaleTimeString());
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      currentTime()
+    }, 1000)
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
+
+  return (
+    <>
+      <h3 className="text-2xl font-extrabold text-center">{language === "en" ? 'Clock' : language === 'it' ? 'Orologio' : 'Wanduhr'}</h3>
+      <p className="text-xl text-center">{time}</p>
+    </>
+  )
+}
+
+export default Clock;
