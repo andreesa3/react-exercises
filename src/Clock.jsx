dc350c8f97be3437e0481012535e75f55047dd86
andreesa3 <90782556+andreesa3@users.noreply.github.com>
2023-10-18 11:33:04 +0200

    Clock.jsx

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export default function Counter() {
+export default function Clock() {
   const [time, setTime] = useState('');
 
   const currentTime = () => {
