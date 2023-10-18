d4e48809178becc5bf99ae79b432cd021875f63f
andreesa3 <90782556+andreesa3@users.noreply.github.com>
2023-10-18 23:30:09 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
       <Container>
-        <h1 className="text-100 text-3xl font-extrabold border-b-2 mb-4 py-2">My Bio</h1>
+        <h1 className="text-3xl font-extrabold border-b-2 mb-4 py-2">My Bio</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis necessitatibus iusto ea pariatur ab assumenda hic, fuga delectus labore id possimus voluptatem facilis architecto eum sunt mollitia magnam amet? Exercitationem?</p>
         <br />
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis necessitatibus iusto ea pariatur ab assumenda hic, fuga delectus labore id possimus voluptatem facilis architecto eum sunt mollitia magnam amet? Exercitationem?</p>
