f476d71f96673f4717e58d3b8f5a790c3d3f8eb6
Andrea <andrea.saitta.as@gmail.com>
2023-10-19 17:50:25 +0200

    ex73

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -1,44 +1,47 @@
 import { useState } from "react";
-import { useEffect } from "react";
 
 const GithubUser = ({ username, setUsername }) => {
-  const API_URL = `https://api.github.com/users/${'andreesa3'}`
+  const API_URL = `https://api.github.com/users/${username}`
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   const handleUsername = (e) => {
     setUsername(e.target.value)
   }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(API_URL);
-        const result = await response.json();
-        console.log(result)
-      } catch (error) {
-        console.error(error);
-      }
+  const fetchData = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error(error.message);
     }
-    fetchData()
-  }, [])
-  
+  }
+
+  const handleSubmitForm = async (e) => {
+    e.preventDefault();
+    setUsername('');
+    await fetchData();
+  }
+
   return (
-    <>
-      <form className="flex justify-center flex-col align-middle h-10 border-red-600">
+    <div className="p-4">
+      <form className="border-red-600" onSubmit={handleSubmitForm}>
         <label htmlFor="username" className="font-bold text-xl">Inserisci nome utente</label>
-        <input type="text" className="border-2 block" name="username" value={username} onChange={handleUsername} />
+        <input type="text" className="border-2 block p-1 mt-2" name="username" value={username} onChange={handleUsername} />
+        <button className="border py-1 px-4 my-3 bg-black text-white hover:bg-gray-800" type="submit">Invia</button>
       </form>
       {
-        // data && (
-        //   data.map(item => {
-        //     return (
-        //       <img src={item.}
-        //     )
-        //   })
-        // )
+        data && (
+          <div className="mt-5 bg-gray-300 p-5 max-w-max">
+            <img className="h-52" src={data.avatar_url} alt="user avatar" />
+            <p className="pt-1 font-extrabold italic">{data.login}</p>
+            <p className="text-2xl">{data.name}</p>
+          </div>
+        )
       }
-    </>
+    </div>
   )
 }
 
