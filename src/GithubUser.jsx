e9db3ab727292606551610f95b7f12251c89f359
Andrea <andrea.saitta.as@gmail.com>
2023-10-19 17:54:02 +0200

    ex73

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -29,7 +29,7 @@ const GithubUser = ({ username, setUsername }) => {
     <div className="p-4">
       <form className="border-red-600" onSubmit={handleSubmitForm}>
         <label htmlFor="username" className="font-bold text-xl">Inserisci nome utente</label>
-        <input type="text" className="border-2 block p-1 mt-2" name="username" value={username} onChange={handleUsername} />
+        <input type="text" className="border-2 block p-1 mt-2" name="username" value={username} onChange={handleUsername} placeholder="andreesa3"/>
         <button className="border py-1 px-4 my-3 bg-black text-white hover:bg-gray-800" type="submit">Invia</button>
       </form>
       {
