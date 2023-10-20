5399e2f89481931b5e952b94e18ed62d92bc33f1
Andrea <andrea.saitta.as@gmail.com>
2023-10-20 23:22:40 +0200

    ex78

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -3,17 +3,21 @@ import useFetch from "./useFetch";
 const GithubUser = ({ username }) => {
   const API_URL = `https://api.github.com/users/${username}`
 
-  const { data } = useFetch(API_URL)
+  const { data, error, loading, fetchData } = useFetch(API_URL)
 
   return (
     <div className="p-4">
+      {loading && <h2 className="text-xl">Loading...</h2>}
+      {error && <p>Utente non trovato</p>}
       {
-        data && (
+        data ? (
           <div className="mt-5 bg-gray-300 p-5 max-w-max">
             <img className="h-52" src={data.avatar_url} alt="user avatar" />
             <p className="pt-1 font-extrabold italic">{data.login}</p>
             <p className="text-2xl">{data.name}</p>
           </div>
+        ) : (
+          <button className="border mt-3 border-black py-1 px-2 hover:bg-black hover:text-white" onClick={fetchData}>Mostra utente</button>
         )
       }
     </div>
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 const useFetch = (API_URL) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   const fetchData = async () => {
+    setLoading(true)
     try {
       const response = await fetch(API_URL);
       const result = await response.json();
-      setData(result);
-      setLoading(false)
+      if (response.ok) {
+        setTimeout(() => {
+          setData(result);
+        }, 500)
+        setLoading(false)
+      }
     } catch (error) {
-      setLoading(false)
       setError(error)
     }
   }
 
-  useEffect(() => {
-    fetchData();
-  }, [])
-
-  return { data }
+  return { data, error, loading, fetchData }
 
 }
 
