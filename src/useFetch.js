420def9398e7482aecbab1a5b739db16f5aa2fc2
Andrea <andrea.saitta.as@gmail.com>
2023-10-20 22:56:52 +0200

    ex77

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,24 +1,24 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-const useFetch = (API_URL) => {
+const useFetch = (username) => {
+  const API_URL = `https://api.github.com/users/${username}`;
   const [data, setData] = useState(null);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(API_URL);
-      const result = await response.json();
-      setData(result);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
     fetchData();
-  }, [])
+  }, [username]);
 
-  return { data }
-
-}
+  return data;
+};
 
 export default useFetch;
