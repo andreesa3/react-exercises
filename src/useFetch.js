4bec8dd237870fdc9773fe2a70be619662c2d020
Andrea <andrea.saitta.as@gmail.com>
2023-10-20 22:51:53 +0200

    ex78

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,14 +2,18 @@ import { useState, useEffect } from "react"
 
 const useFetch = (API_URL) => {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true)
 
   const fetchData = async () => {
     try {
       const response = await fetch(API_URL);
       const result = await response.json();
       setData(result);
+      setLoading(false)
     } catch (error) {
-      console.error(error.message);
+      setLoading(false)
+      setError(error)
     }
   }
 
