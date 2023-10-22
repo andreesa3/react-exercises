c6ac9567a16c42510415335761256e9fffe57cf5
Andrea <andrea.saitta.as@gmail.com>
2023-10-22 16:16:01 +0200

    ex82

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import FilteredList from "./FilteredList";
+import { Routes, Route } from "react-router-dom";
+import Welcome from "./Welcome";
 
 const App = () => {
-  const people = [
-    { id: 1, name: "John", age: 19 },
-    { id: 2, name: "Alice", age: 30 },
-    { id: 3, name: "Bob", age: 22 },
-    { id: 4, name: "Mario", age: 18 },
-    { id: 5, name: "Marco", age: 17 },
-    { id: 6, name: "Luca", age: 16 },
-  ];
-  
+
   return (
     <>
-      <FilteredList list={people} />
+      <Routes>
+        <Route path="/" element={<Welcome name="Andrea" />} />
+      </Routes>
     </>
   )
 }
