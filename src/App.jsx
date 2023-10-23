3c233597bfec51e2c600540df96ac623fa826f76
Andrea <andrea.saitta.as@gmail.com>
2023-10-23 12:09:16 +0200

    ex84

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Counter from "./pages/Counter";
+import ShowGithubUser from "./pages/ShowGithubUser";
 import Welcome from "./pages/Welcome";
 
 const App = () => {
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<Welcome name="Andrea" />} />
+        <Route path="/" element={<Welcome name="User" />} />
         <Route path="/counter" element={<Counter />} />
-        <Route path="users/:username" element={<ShowGithubUser username="andreesa3" />} />
+        <Route path="users/:username" element={<ShowGithubUser />} />
       </Routes>
     </>
   )
--- src/pages/ShowGithubUser.jsx
+++ src/pages/ShowGithubUser.jsx
@@ -1,7 +1,14 @@
-const ShowGithubUser = ({ username }) => {
+import { useParams } from 'react-router-dom';
+import GithubUser from './GithubUser'
+
+const ShowGithubUser = () => {
+  const { username } = useParams();
+
   return (
     <>
       <GithubUser username={username} />
     </>
   )
 }
+
+export default ShowGithubUser;
