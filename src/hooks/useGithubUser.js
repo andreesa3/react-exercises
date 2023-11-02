8a5959f6f50641b84ed1bbd7b6b1ef35337f43f0
Andrea <andrea.saitta.as@gmail.com>
2023-11-02 12:21:54 +0100

    ex92

--- src/hooks/useGithubUser.js
+++ src/hooks/useGithubUser.js
@@ -1,10 +1,9 @@
 import useSWR from 'swr'
 
 const useGithubUser = (API_URL, username) => {
-  const fetcher = (url) => fetch(url).then(res => res.json());
 
   if (username) {
-    const { data, error, isLoading, mutate } = useSWR(API_URL, fetcher)
+    const { data, error, isLoading, mutate } = useSWR(API_URL)
 
     const handleRefresh = () => {
       mutate()
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { SWRConfig } from 'swr';
 import App from './App';
 import './index.css'
 
+const fetcher = url => fetch(url).then(res => res.json());
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <SWRConfig value={{ fetcher }}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </SWRConfig>
 )
